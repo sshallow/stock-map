daae36c55c2520d5a5235f49c1ff8fbf27034c3a
shangshuai_company <doit_2017@163.com>
2023-12-28 14:21:43 +0800

    default dark

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
 
-import { Button } from '@/components/ui/button';
 import Link from 'next/link';
-import NavBar from '../components/NavBar';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <NavBar />
+      {/* <NavBar /> */}
       <section className='py-24 flex flex-col items-center text-center gap-8'>
         <h1 className='text-4xl font-bold'>穿透迷雾,洞悉股海</h1>
         <p className='text-2xl text-muted-foreground'>市场全景,一览无余</p>
 
 
         {/* 倒序 100 个数 */}
-        <div className='flex flex-col gap-4'>
+        {/* <div className='flex flex-col gap-4'>
           <div className='flex gap-4'>
             <div className='flex flex-col gap-2'>
               <p className='text-2xl font-bold'>100</p>
@@ -24,17 +22,17 @@ export default function Home() {
               <p className='text-2xl text-muted-foreground'>股票</p>
             </div>
           </div>
-        </div>
+        </div> */}
 
       </section>
       <div className='flex gap-6 items-center'>
-        <Button variant={"secondary"}>
+        {/* <Button variant={"secondary"}>
           开始使用
-        </Button>
+        </Button> */}
 
         <Link href='/map'>
           {/* <Button > */}
-          开始使用
+          开始探索
           {/* </Button> */}
         </Link>
 
