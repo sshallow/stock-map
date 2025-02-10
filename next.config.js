3fb6f47a1d3e3093b61e171282e9082a86c2501d
shaun <doit_2017@outlook.com>
2025-02-10 13:50:36 +0800

    fix http proxy

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://smartpayt.com/prod-api/:path*'
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
